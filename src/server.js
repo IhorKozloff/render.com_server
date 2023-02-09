dd212eda3cf6267fc465afd59d71a2c2cb3eac23
IhorKozloff <ik.eyeshield21@gmail.com>
2023-02-09 18:36:44 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 const { PORT = 3005, MESSAGE, MONGO_URL } = process.env;
 
-// mongoose
-// .connect(MONGO_URL).then(app.listen(PORT, () => {
-//         console.log(`${MESSAGE} ${PORT}!`);
-//     }))
+mongoose
+.connect(MONGO_URL).then(app.listen(PORT, () => {
+        console.log(`${MESSAGE} ${PORT}!`);
+    }))
 
-app.listen(PORT, () => {
-    console.log(`${MESSAGE} ${PORT}!`);
-})
+// app.listen(PORT, () => {
+//     console.log(`${MESSAGE} ${PORT}!`);
+// })
